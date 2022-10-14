8d121aff814a688a3ab0c53a8f1fdd5c622d0549
KinaraGeorge <ggkinara@gmail.com>
2022-10-14 12:27:41 +0000

    c program that prints out using 'printf'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints phrase using printf
+ * Return: executed if runs as expected
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
