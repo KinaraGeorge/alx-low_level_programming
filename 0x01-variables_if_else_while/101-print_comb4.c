e312d912ba01bd1fe7cc93a8440b6d034edba93c
KinaraGeorge <54885174+KinaraGeorge@users.noreply.github.com>
2022-10-16 15:41:33 +0300

    Update 101-print_comb4.c
    prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,11 +6,28 @@
  */
 int main(void)
 {
-	int a, b, c;
+	int n, m, l;
 
-	a = 48;
-	b = 48;
-	c = 48;
-
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
 	return (0);
 }
