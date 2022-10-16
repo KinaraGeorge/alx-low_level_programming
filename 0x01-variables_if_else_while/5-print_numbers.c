09df749c95d2766f8909a3fb3ebd1323e4a64cd5
KinaraGeorge <ggkinara@gmail.com>
2022-10-16 10:09:37 +0000

    prints all base 10 numbers starting with 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints all base 10 number starting from 0
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
