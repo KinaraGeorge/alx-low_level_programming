facdf22bb426b73a80d96ea4802d3520f5ea8c1f
KinaraGeorge <54885174+KinaraGeorge@users.noreply.github.com>
2022-10-16 11:50:26 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,21 +1,30 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-/*
- * main prints a random number and tells us what is the lat digit
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if the last digit of a random number
+ * Return: 0 on success
  */
 int main(void)
 {
 	int n;
+	char last[] = "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is %d \n", n, n % 10);
+	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
-		printf("and is greater than 5\n");
+	{
+		printf("greater than 5\n");
+	}
 	else if (n % 10 == 0)
-		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
 	return (0);
+
 }
