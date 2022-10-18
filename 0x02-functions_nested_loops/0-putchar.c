d93fbd4779d4259adf72a5690e8c11f2abed9305
KinaraGeorge <ggkinara@gmail.com>
2022-10-18 11:06:27 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,10 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 8; i++)
+	for (int i = 0; i < 8; i++)
 	{
+		char text[9] = "_putchar";
+
 		_putchar(text[i]);
 	}
 	_putchar('\n');
