d8c1041278f9d79ed45c3bc8bbda2012f6cd2f78
KinaraGeorge <ggkinara@gmail.com>
2022-10-18 14:23:18 +0000

    checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: c is an ascii character;
  * Return: 1 if letter and 0 if not a letter
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 	{
